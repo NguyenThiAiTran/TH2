4b223d3969858e0c00ad75f6d4424126a60d14b9
Nguyen Thi Ai Tran <ntatran2101259@student.ctuet.edu.vn>
2024-10-03 14:20:36 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Hello, HelloPerson } from './Hello'; // Sửa ở đây
+import { Header } from './Header';
+import Car from './Car';
+import Login from './Login'; // Thêm dòng này
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const studentName = "Nguyễn Thị Ái Trân"; // Thay đổi tên sinh viên tại đây
+
+    return (
+        <div>
+            <Header />
+            <Hello />
+            <HelloPerson name={studentName} />
+            <Car />
+            <Login /> {/* Gọi component Login */}
+        </div>
+    );
 }
 
 export default App;
